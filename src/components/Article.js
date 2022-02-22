9769ed382d4e588e2fad69953bba78c51025dd8b
Zuzanna Komar <zkomar@deloittece.com>
2022-02-22 13:00:53 +0100

    Updates

--- src/components/Article.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Article = ({ article }) => {
-  return (
-    <div className="article" dangerouslySetInnerHTML={{ __html: article }} />
-  );
-};
-
-export default Article;
